a0fdc35e765bdc6312bfd32e9434ac714dd092ab
Anonymous <>
2022-03-29 17:32:06 +0200

    Initial commit - Creating the project

---
+++ src/main/java/com/digitalstork/devbookskata/DevbookskataApplication.java
@@ -0,0 +1,13 @@
+package com.digitalstork.devbookskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbookskataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbookskataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalstork/devbookskata/DevbookskataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalstork.devbookskata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbookskataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
