e1ae0820b66638d384d3cc9173e24d293341379d
Anonymous <>
2022-03-30 12:22:14 +0200

    Implement mapper developmentBookToDevelopmentBookListDto method

--- src/main/java/com/digitalstork/devbookskata/mapper/DevelopmentBookDevelopmentBookDtoMapper.java
+++ src/main/java/com/digitalstork/devbookskata/mapper/DevelopmentBookDevelopmentBookDtoMapper.java
@@ -3,9 +3,11 @@ package com.digitalstork.devbookskata.mapper;
 import com.digitalstork.devbookskata.dto.DevelopmentBookListDto;
 import com.digitalstork.devbookskata.model.DevelopmentBook;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface DevelopmentBookDevelopmentBookDtoMapper {
 
+    @Mapping(target = "reference", expression = "java(new StringBuilder(\"\").append(developmentBook.getAuthor()).append(\", \").append(developmentBook.getEdition()).toString())")
     DevelopmentBookListDto developmentBookToDevelopmentBookListDto(DevelopmentBook developmentBook);
 }
