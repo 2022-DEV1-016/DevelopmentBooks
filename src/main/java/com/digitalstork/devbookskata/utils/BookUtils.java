63038d44ad89366f341db14a2bdb47b87c53f1b7
Anonymous <>
2022-03-31 21:33:09 +0200

    Implement calculatePrice method for one book

--- src/main/java/com/digitalstork/devbookskata/utils/BookUtils.java
+++ src/main/java/com/digitalstork/devbookskata/utils/BookUtils.java
@@ -1,11 +1,22 @@
 package com.digitalstork.devbookskata.utils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class BookUtils {
 
+    public static final Integer singleBookPrice = 50;
+
     public static Integer calculatePrice(List<Set<Long>> bookPacks) {
-        return null;
+        if (Objects.nonNull(bookPacks) && bookPacks.size() == 1) {
+            if (bookPacks.get(0).size() == 1) {
+                return singleBookPrice;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
