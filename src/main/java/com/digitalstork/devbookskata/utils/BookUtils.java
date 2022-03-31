633f1532c0871999d3c57753642f90b2ab861ad4
Anonymous <>
2022-03-31 22:24:01 +0200

    [ Update ] - Implement calculatePrice method for three books pack

--- src/main/java/com/digitalstork/devbookskata/utils/BookUtils.java
+++ src/main/java/com/digitalstork/devbookskata/utils/BookUtils.java
@@ -8,15 +8,26 @@ public class BookUtils {
 
     public static final Integer singleBookPrice = 50;
 
+    public static final Integer threeBooksDiscount = 10;
+
     public static Integer calculatePrice(List<Set<Long>> bookPacks) {
         if (Objects.nonNull(bookPacks) && bookPacks.size() == 1) {
             if (bookPacks.get(0).size() == 1) {
                 return singleBookPrice;
             } else {
-                return null;
+                return calculatePriceWithDiscount(bookPacks.get(0).size());
             }
         } else {
             return null;
+        }
+    }
+
+    private static Integer calculatePriceWithDiscount(Integer numberOfBooks) {
+        switch (numberOfBooks) {
+            case 3:
+                return singleBookPrice * 3 - singleBookPrice * 3 * threeBooksDiscount / 100;
+            default:
+                return null;
         }
     }
 }
