d734c115756d011d14dfd1d5bdb6b11946151310
Anonymous <>
2022-03-31 22:43:21 +0200

    [ Test ] - calculatePrice unit test for all 4 different packs

--- src/test/java/com/digitalstork/devbookskata/utils/BookUtilsTest.java
+++ src/test/java/com/digitalstork/devbookskata/utils/BookUtilsTest.java
@@ -3,10 +3,7 @@ package com.digitalstork.devbookskata.utils;
 import com.digitalstork.devbookskata.model.DevelopmentBook;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,6 +35,33 @@ class BookUtilsTest {
 
         //When
         Integer resultPrice = BookUtils.calculatePrice(Arrays.asList(threeBooksPack));
+
+        //Assert
+        assertNotNull(resultPrice);
+        assertEquals(expectedPrice, resultPrice);
+    }
+
+    @Test
+    void shouldApplyAllFourDiscountTypes_onFourBookPacksRespectively_andReturnTotalPrice() {
+        //Given
+        Set<Long> twoBooksPack = new HashSet<Long>(Arrays.asList(1L, 2L));
+        Set<Long> threeBooksPack = new HashSet<Long>(Arrays.asList(1L, 2L, 3L));
+        Set<Long> fourBooksPack = new HashSet<Long>(Arrays.asList(1L, 2L, 3L, 4L));
+        Set<Long> fiveBooksPack = new HashSet<Long>(Arrays.asList(1L, 2L, 3L, 4L, 5L));
+
+        List<Set<Long>> packList = new ArrayList<>();
+        packList.add(twoBooksPack);
+        packList.add(threeBooksPack);
+        packList.add(fourBooksPack);
+        packList.add(fiveBooksPack);
+
+        Integer expectedPrice = 2 * singleBookPrice -  2 * singleBookPrice * 5 / 100
+                              + 3 * singleBookPrice -  3 * singleBookPrice * 10 / 100
+                              + 4 * singleBookPrice -  4 * singleBookPrice * 20 / 100
+                              + 5 * singleBookPrice -  5 * singleBookPrice * 25 / 100;
+
+        //When
+        Integer resultPrice = BookUtils.calculatePrice(packList);
 
         //Assert
         assertNotNull(resultPrice);
