447c37b27ca2014696b2b26572ea0e208f7f2209
Anonymous <>
2022-04-01 19:31:17 +0200

    update initial data script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 
 insert into development_book (id, title, author, edition, nb_available_copies)
-            values (1, 'Clean Code', 'Robert Martin', 2008, 5);
+            values (1, 'Clean Code', 'Robert Martin', 2008, 20);
 
 insert into development_book (id, title, author, edition, nb_available_copies)
-            values (2, 'The Clean Coder', 'Robert Martin', 2011, 5);
+            values (2, 'The Clean Coder', 'Robert Martin', 2011, 20);
 
 insert into development_book (id, title, author, edition, nb_available_copies)
-            values (3, 'Clean Architecture', 'Robert Martin', 2017, 5);
+            values (3, 'Clean Architecture', 'Robert Martin', 2017, 20);
 
 insert into development_book (id, title, author, edition, nb_available_copies)
-            values (4, 'Test Driven Development by Example', 'Kent Beck', 2003, 5);
+            values (4, 'Test Driven Development by Example', 'Kent Beck', 2003, 20);
 
 insert into development_book (id, title, author, edition, nb_available_copies)
-            values (5, 'Working Effectively With Legacy Code', 'Michael C. Feathers', 2004, 5);
+            values (5, 'Working Effectively With Legacy Code', 'Michael C. Feathers', 2004, 20);
