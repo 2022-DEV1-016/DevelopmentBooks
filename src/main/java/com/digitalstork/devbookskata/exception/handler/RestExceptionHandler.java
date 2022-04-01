7210bf8dd527c8727996903c26493d6908e828f0
Anonymous <>
2022-04-01 15:06:14 +0200

    Implement internal exceptions handling

--- src/main/java/com/digitalstork/devbookskata/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/digitalstork/devbookskata/exception/handler/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.digitalstork.devbookskata.exception.handler;
 
 import com.digitalstork.devbookskata.exception.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
@@ -44,5 +46,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         return ResponseEntity.badRequest().body(response);
 
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleTechnicalException(Exception ex) {
+        log.error("Technical Exception : {}", ex);
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .errorCode("")
+                .errorMsg("")
+                .build();
+
+        return ResponseEntity.internalServerError().body(response);
     }
 }
