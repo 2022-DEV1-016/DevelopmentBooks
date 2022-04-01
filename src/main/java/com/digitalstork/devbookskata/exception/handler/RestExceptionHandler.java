ef85b7a9deae340ff36331df34337591a859f746
Anonymous <>
2022-04-01 14:10:04 +0200

    Implement   NoAvailableBooksException handling

--- src/main/java/com/digitalstork/devbookskata/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/digitalstork/devbookskata/exception/handler/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package com.digitalstork.devbookskata.exception.handler;
 import com.digitalstork.devbookskata.exception.BookNotFoundException;
 import com.digitalstork.devbookskata.exception.ErrorCodeE;
 import com.digitalstork.devbookskata.exception.ErrorResponse;
+import com.digitalstork.devbookskata.exception.NoAvailableBooksException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +18,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponse response = ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .errorCode(ErrorCodeE.BOOK_NOT_FOUND.name())
+                .errorMsg(ex.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest().body(response);
+
+    }
+
+    @ExceptionHandler(NoAvailableBooksException.class)
+    public ResponseEntity<ErrorResponse> handleNoAvailableBooksException(NoAvailableBooksException ex) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .errorCode(ErrorCodeE.BOOK_OUT_OF_STOCK.name())
                 .errorMsg(ex.getMessage())
                 .build();
 
